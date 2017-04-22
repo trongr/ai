baf56999db35829b5ac7a1533e2b6678a91b0d83
trong <trongr@users.noreply.github.com>
2017-04-22 02:26:16 -0400

    tf arrays

--- tensorflow/arrays.py
+++ tensorflow/arrays.py
@@ -20,3 +20,22 @@ with tf.Session() as session:
     print "Matrix (3x3 entries):\n %s \n" % result
     result = session.run(Tensor)
     print "Tensor (3x3x3 entries) :\n %s \n" % result
+
+Matrix_one = tf.constant([[1,2,3],[2,3,4],[3,4,5]])
+Matrix_two = tf.constant([[2,2,2],[2,2,2],[2,2,2]])
+
+first_operation = tf.add(Matrix_one, Matrix_two)
+second_operation = Matrix_one + Matrix_two
+arraysAreEqual = tf.reduce_all(tf.equal(first_operation, second_operation, name=None))
+
+with tf.Session() as session:
+    result1 = session.run(first_operation)
+    print "Defined using tensorflow function :"
+    print(result1)
+    result2 = session.run(second_operation)
+    print "Defined using normal expressions :"
+    print(result2)
+
+    arraysAreEqualResult = session.run(arraysAreEqual)
+
+    print("Are these two arrays the same?", arraysAreEqualResult)
