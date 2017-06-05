41dfa0f1739a1c486a26ff20ad64f188a2cb1104
trong <trongr@users.noreply.github.com>
2017-06-05 00:10:07 -0400

    Char RNN: generate train batch data when needed instead of all at the beginning

--- tensorflow/rnn/charrnn.py
+++ tensorflow/rnn/charrnn.py
@@ -9,7 +9,6 @@ import tensorflow as tf
 
 FILENAME = sys.argv[1]
 DATA = open(FILENAME, 'r').read()
-DATA = DATA[:1000000]  # poij toggle truncate data
 CHARS = list(set(DATA))
 DATA_SIZE, VOCAB_SIZE = len(DATA), len(CHARS)
 CHAR_TO_IX = {ch: i for i, ch in enumerate(CHARS)}
@@ -22,16 +21,16 @@ print "CHAR_TO_IX", CHAR_TO_IX
 print "IX_TO_CHAR", IX_TO_CHAR
 
 BATCH_SIZE = 100
-SEQ_LENGTH = 20
-NUM_CELL_UNITS = 25
-NUM_LSTM_CELLS = 1
+SEQ_LENGTH = 10
+NUM_CELL_UNITS = 1024
+NUM_LSTM_CELLS = 3
 NUM_CLASSES = len(CHARS)
 
 """
 Generate training input and convert into an array of shape (None, SEQ_LENGTH,
 1):
 
-dataX = [
+batchX = [
     [[1], [0], [1], [1],...],
     [[0], [0], [0], [1],...],
     [[1], [1], [1], [1],...],
@@ -41,46 +40,46 @@ dataX = [
 Generate training output and convert into one-hot array of shape (None,
 NUM_CLASSES):
 
-dataY = [
+batchY = [
     [0,..., 0, 1, 0,..., 0],
     [1,..., 0, 0, 0,..., 0],
     [0,..., 0, 0, 0,..., 1],
     ...
 ] ~ (None, NUM_CLASSES)
 """
-
-
-def genData():
-    dataX = []
-    dataY = []
-
-    NUM_DATA = len(DATA) - SEQ_LENGTH
-    idx = range(NUM_DATA)
-    # shuffle(idx)
-    for i in idx:
-        x = [[CHAR_TO_IX[ch]] for ch in list(DATA[i:i + SEQ_LENGTH])]
-        y = CHAR_TO_IX[DATA[i + SEQ_LENGTH]]
-        dataX.append(x)
-        dataY.append(y)
+SIZE_DATA = len(DATA) - SEQ_LENGTH # - SEQ_LENGTH to avoid clipping (short strings) near the end
+NUM_TRAIN = int(0.8 * SIZE_DATA) # 80-20 train-test split
+batch_ptr = 0 # pointer to start char in DATA for current batch
+def nextTrainBatch():
+    global batch_ptr
+    batchX = []
+    batchY = []
+    for i in xrange(BATCH_SIZE):
+        x = [[CHAR_TO_IX[ch]] for ch in list(DATA[batch_ptr:batch_ptr + SEQ_LENGTH])]
+        y = CHAR_TO_IX[DATA[batch_ptr + SEQ_LENGTH]]
+        batchX.append(x)
+        batchY.append(y)
+        batch_ptr = (batch_ptr + 1) % NUM_TRAIN # loop around for further training    
 
     # Converting dataY values into one-hot
-    zeros = np.zeros((len(dataY), NUM_CLASSES))
-    zeros[np.arange(len(dataY)), dataY] = 1.0
-    dataY = zeros
+    zeros = np.zeros((len(batchY), NUM_CLASSES))
+    zeros[np.arange(len(batchY)), batchY] = 1.0
+    batchY = zeros
 
-    dataX = np.array(dataX)
-    dataY = np.array(dataY)
-    return dataX, dataY
+    batchX = np.array(batchX)
+    batchY = np.array(batchY)
+
+    return batchX, batchY
 
 def genTestXFromString(s):
     ixes = [[[CHAR_TO_IX[ch]] for ch in s]]
     return ixes
 
 def sample():
-    s = "what is the meaning of life?"
+    s = "To be, or not to be- that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune Or to take arms against a sea of troubles, And by opposing end them."
     s = s[:SEQ_LENGTH] # make it fit our model's input
     output = []
-    GEN_STR_LEN = 200
+    GEN_STR_LEN = 300
     for i in xrange(GEN_STR_LEN):
         testX = genTestXFromString(s)
         pred = sess.run(Pred, {X: testX})
@@ -108,26 +107,12 @@ def char_distr_to_char_ix(pred):
 def char_distr_to_char(pred):
     return IX_TO_CHAR[char_distr_to_char_ix(pred)]
 
-
 def print_test_results(testX, pred):
-    LENGTH = len(testX)
-    for i in xrange(LENGTH):
+    for i in xrange(len(testX)):
         test_str = testX[i].reshape(-1)
         char_distr = pred[i].reshape(-1)
         predicted_char = char_distr_to_char(char_distr)
         print ixes_to_string(test_str), "\t", predicted_char
-
-
-dataX, dataY = genData()
-
-"""
-Separating training and test data.
-"""
-NUM_TRAIN = int(0.8 * len(dataX))
-trainX = dataX[:NUM_TRAIN]
-trainY = dataY[:NUM_TRAIN]
-testX = dataX[NUM_TRAIN:]
-testY = dataY[NUM_TRAIN:]
 
 """
 Placeholders for minibatch input and output data
@@ -142,20 +127,34 @@ BUILDING THE GRAPH
 """
 
 """
-RNN layer
+Pass all activations to FC layer
 """
+"""
+# RNN layer
 Cell = tf.contrib.rnn.BasicLSTMCell(NUM_CELL_UNITS, state_is_tuple=True)
 Cells = tf.contrib.rnn.MultiRNNCell([Cell] * NUM_LSTM_CELLS)
 Output, State = tf.nn.dynamic_rnn(Cells, X, dtype=tf.float32)
-Output = tf.reshape(Output, [tf.shape(Output)[0], -1])
+Output = tf.reshape(Output, [tf.shape(Output)[0], -1]) 
 
-"""
-Fully connected layer mapping RNN output to classes
-"""
-W = tf.Variable(tf.truncated_normal(
-    [SEQ_LENGTH * NUM_CELL_UNITS, NUM_CLASSES]))
+# Fully connected layer mapping RNN output to classes
+W = tf.Variable(tf.truncated_normal([SEQ_LENGTH * NUM_CELL_UNITS, NUM_CLASSES]))
 b = tf.Variable(tf.constant(0.1, shape=[NUM_CLASSES]))
 Scores = tf.matmul(Output, W) + b
+"""
+
+"""
+Only pass the last activations
+"""
+# RNN layer
+Cell = tf.contrib.rnn.BasicLSTMCell(NUM_CELL_UNITS, state_is_tuple=True)
+Cells = tf.contrib.rnn.MultiRNNCell([Cell] * NUM_LSTM_CELLS)
+Output, State = tf.nn.dynamic_rnn(Cells, X, dtype=tf.float32)
+Last = tf.gather(tf.transpose(Output, [1, 0, 2]), SEQ_LENGTH - 1) 
+
+# Fully connected layer mapping RNN output to classes
+W = tf.Variable(tf.truncated_normal([NUM_CELL_UNITS, NUM_CLASSES]))
+b = tf.Variable(tf.constant(0.1, shape=[NUM_CLASSES]))
+Scores = tf.matmul(Last, W) + b
 
 """
 Loss and optimization
@@ -172,33 +171,26 @@ TRAINING
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-EPOCHS = 1000
-NUM_BATCHES = int(NUM_TRAIN / BATCH_SIZE)
-for i in range(EPOCHS):
-    print "EPOCH", i
-    ptr = 0
-    for j in range(NUM_BATCHES):
-        batchX = trainX[ptr:ptr + BATCH_SIZE]
-        batchY = trainY[ptr:ptr + BATCH_SIZE]
-        minimize, loss, output, state = sess.run(
-            [Minimize, Loss, Output, State], {X: batchX, Y: batchY})
+i = 0
+while True: 
+    batchX, batchY = nextTrainBatch()
+    minimize, loss, state = sess.run([Minimize, Loss, State], {X: batchX, Y: batchY})
 
-        if j % 100 == 0:
-            accuracy = sess.run(Accuracy, {X: batchX, Y: batchY})
-            print('Batch: {:2d}, loss: {:.4f}, accuracy: {:3.1f} %'.format(
-                j, loss, 100 * accuracy))
-            sample()
+    if i % 50 == 0:
+        accuracy = sess.run(Accuracy, {X: batchX, Y: batchY})
+        print('Batch: {:2d}, loss: {:.4f}, accuracy: {:3.1f} %'.format(i, loss, 100 * accuracy))
+        sample()
 
-        ptr += BATCH_SIZE
+    i += 1
 
-"""
-TESTING
-"""
-accuracy = sess.run(Accuracy, {X: testX, Y: testY})
-print('Accuracy on test set: {:3.1f} %'.format(100 * accuracy))
+# """
+# TESTING. TODO. generate testX testY from DATA similar to nextTrainBatch()
+# """
+# accuracy = sess.run(Accuracy, {X: testX, Y: testY})
+# print('Accuracy on test set: {:3.1f} %'.format(100 * accuracy))
 
-# Don't need to pass testY in here because Pred doesn't need it:
-pred = sess.run(Pred, {X: testX})
-print_test_results(testX, pred)
+# # Don't need to pass testY in here because Pred doesn't need it:
+# pred = sess.run(Pred, {X: testX})
+# print_test_results(testX, pred)
 
 sess.close()
