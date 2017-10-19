6ce54406a2cc9c9b5090b19bac6908ffb87946de
trong <trongr@users.noreply.github.com>
2017-10-19 00:16:59 -0400

    Audio RNN: printing out the whole batch so we know exactly what is being trained instead of just the first sentence

--- audio/audio_abc.py
+++ audio/audio_abc.py
@@ -6,6 +6,10 @@ Generate ABC training data by running:
 
 This will concatenate all the ABC files in data/abc/ into one master file (data/abc/master.abc).
 
+Optionally preprocess master.abc with
+
+    python bin\preprocess_abc.py data\abc\master.abc > data\abc\master_processed.abc
+
 Then run this file to train and generate audio:
 
     python audio_abc.py data/abc/master.abc
@@ -52,6 +56,11 @@ while True:
     batchX, batchY = model.train_batch()
 
     if i % 1 == 0:
+        print("TRAINING")
+        print("========")
+        print(utils.batchX_to_str(IX_TO_CHAR, batchX))
+        print("========")
+
         print('Batch: {:2d}, elapsed: {:.4f}'.format(i, time.time() - t))
         t = time.time()
 
@@ -61,15 +70,21 @@ while True:
         print('Batch: {:2d}, elapsed: {:.4f}, loss: {:.4f}, accuracy: {:3.1f} %'.format(i, time.time() - t, loss, 100 * accuracy))
         t = time.time()
 
-    if i % 500 == 0:    
+    """
+    GENERATE AUDIO.
+
+    Once the network has learned enough, generate a music sample every iteration and 
+    see if it's simply repeating (with slight variation) what is currently being trained
+    right now.
+
+    Another test to try: once the network has learned enough, let
+    the network generate on its own without training at the same time.
+    """
+    # if i % 1 == 0:
+    if i % 50 == 0:    
     # if i % 50 == 0 and i != 0:    
         sample = model.sample(100)
         sample = utils.ixes_to_string(IX_TO_CHAR, sample)
-
-        print("TRAINING")
-        print("========")
-        print(utils.batchX_to_str(IX_TO_CHAR, batchX))
-        print("========")
 
         print("SAMPLE")
         print("======")
