7974705b9bafe0e58a8b759125585fb96ba5db9f
trong <trongr@users.noreply.github.com>
2017-12-17 18:30:11 -0500

    Autoencoder: denoising images

--- autoencoder/autoencoder_denoise.py
+++ autoencoder/autoencoder_denoise.py
@@ -15,53 +15,57 @@ x_test = x_test.astype('float32') / 255.
 x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))  # adapt this if using `channels_first` image data format
 x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))  # adapt this if using `channels_first` image data format
 
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
+x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+
 input_img = Input(shape=(28, 28, 1))  # adapt this if using `channels_first` image data format
 
-x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
 x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+# at this point the representation is (7, 7, 32)
 
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
-x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(16, (3, 3), activation='relu')(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
 x = UpSampling2D((2, 2))(x)
 decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
-ENCODER_MODEL_SAVE_PATH = 'convautoencoder_encoder.h5'
-AUTOENCODER_MODEL_SAVE_PATH = 'convautoencoder_autoencoder.h5'
+ENCODER_MODEL_SAVE_PATH = 'autoencoder_denoise_encoder.h5'
+AUTOENCODER_MODEL_SAVE_PATH = 'autoencoder_denoise_autoencoder.h5'
 if os.path.isfile(AUTOENCODER_MODEL_SAVE_PATH):
     encoder = load_model(ENCODER_MODEL_SAVE_PATH)
     autoencoder = load_model(AUTOENCODER_MODEL_SAVE_PATH)
 else:
     encoder = Model(input_img, encoded)
     autoencoder = Model(input_img, decoded)
-    
+
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
-autoencoder.fit(x_train, x_train,
+autoencoder.fit(x_train_noisy, x_train,
                 epochs=50, # TODO adjust
                 batch_size=128,
                 shuffle=True,
-                validation_data=(x_test, x_test),
+                validation_data=(x_test_noisy, x_test),
                 callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
+                
 encoder.save(ENCODER_MODEL_SAVE_PATH)
 autoencoder.save(AUTOENCODER_MODEL_SAVE_PATH)
 
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = autoencoder.predict(x_test)
+encoded_imgs = encoder.predict(x_test_noisy)
+decoded_imgs = autoencoder.predict(x_test_noisy)
 
 n = 10
 plt.figure(figsize=(20, 4))
 for i in range(n):
     # display original
     ax = plt.subplot(2, n, i + 1)
-    plt.imshow(x_test[i].reshape(28, 28))
+    plt.imshow(x_test_noisy[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
@@ -74,14 +78,4 @@ for i in range(n):
     ax.get_yaxis().set_visible(False)
 plt.show()
 
-# Display encoding
-n = 10
-plt.figure(figsize=(20, 8))
-for i in range(n):
-    ax = plt.subplot(1, n, i + 1)
-    plt.imshow(encoded_imgs[i].reshape(4, 4 * 8).T)
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.show()
 
