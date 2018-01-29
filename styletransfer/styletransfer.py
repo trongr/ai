5c2fea2875a2ab419cb28d7f25c6f1b0c0b94f7c
trong <trongr@users.noreply.github.com>
2018-01-29 01:05:08 -0500

    Style transfer

--- styletransfer/styletransfer.py
+++ styletransfer/styletransfer.py
@@ -170,3 +170,112 @@ def tv_loss_test(correct):
     print('Error is {:.3f}'.format(error))
 
 tv_loss_test(answers['tv_out'])
+
+def style_transfer(content_image, style_image, image_size, style_size, content_layer, content_weight,
+                   style_layers, style_weights, tv_weight, init_random = False):
+    """Run style transfer!
+    
+    Inputs:
+    - content_image: filename of content image
+    - style_image: filename of style image
+    - image_size: size of smallest image dimension (used for content loss and generated image)
+    - style_size: size of smallest style image dimension
+    - content_layer: layer to use for content loss
+    - content_weight: weighting on content loss
+    - style_layers: list of layers to use for style loss
+    - style_weights: list of weights to use for each layer in style_layers
+    - tv_weight: weight of total variation regularization term
+    - init_random: initialize the starting image to uniform random noise
+    """
+    # Extract features from the content image
+    content_img = preprocess_image(load_image(content_image, size=image_size))
+    feats = model.extract_features(model.image)
+    content_target = sess.run(feats[content_layer],
+                              {model.image: content_img[None]})
+
+    # Extract features from the style image
+    style_img = preprocess_image(load_image(style_image, size=style_size))
+    style_feat_vars = [feats[idx] for idx in style_layers]
+    style_target_vars = []
+    # Compute list of TensorFlow Gram matrices
+    for style_feat_var in style_feat_vars:
+        style_target_vars.append(gram_matrix(style_feat_var))
+    # Compute list of NumPy Gram matrices by evaluating the TensorFlow graph on the style image
+    style_targets = sess.run(style_target_vars, {model.image: style_img[None]})
+
+    # Initialize generated image to content image
+    
+    if init_random:
+        img_var = tf.Variable(tf.random_uniform(content_img[None].shape, 0, 1), name="image")
+    else:
+        img_var = tf.Variable(content_img[None], name="image")
+
+    # Extract features on generated image
+    feats = model.extract_features(img_var)
+    # Compute loss
+    c_loss = content_loss(content_weight, feats[content_layer], content_target)
+    s_loss = style_loss(feats, style_layers, style_targets, style_weights)
+    t_loss = tv_loss(img_var, tv_weight)
+    loss = c_loss + s_loss + t_loss
+    
+    # Set up optimization hyperparameters
+    initial_lr = 3.0
+    decayed_lr = 0.1
+    decay_lr_at = 180
+    max_iter = 200
+
+    # Create and initialize the Adam optimizer
+    lr_var = tf.Variable(initial_lr, name="lr")
+    # Create train_op that updates the generated image when run
+    with tf.variable_scope("optimizer") as opt_scope:
+        train_op = tf.train.AdamOptimizer(lr_var).minimize(loss, var_list=[img_var])
+    # Initialize the generated image and optimization variables
+    opt_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=opt_scope.name)
+    sess.run(tf.variables_initializer([lr_var, img_var] + opt_vars))
+    # Create an op that will clamp the image values when run
+    clamp_image_op = tf.assign(img_var, tf.clip_by_value(img_var, -1.5, 1.5))
+    
+    f, axarr = plt.subplots(1,2)
+    axarr[0].axis('off')
+    axarr[1].axis('off')
+    axarr[0].set_title('Content Source Img.')
+    axarr[1].set_title('Style Source Img.')
+    axarr[0].imshow(deprocess_image(content_img))
+    axarr[1].imshow(deprocess_image(style_img))
+    plt.show()
+    plt.figure()
+    
+    # Hardcoded handcrafted 
+    for t in range(max_iter):
+        # Take an optimization step to update img_var
+        sess.run(train_op)
+        if t < decay_lr_at:
+            sess.run(clamp_image_op)
+        if t == decay_lr_at:
+            sess.run(tf.assign(lr_var, decayed_lr))
+        if t % 100 == 0:
+            print('Iteration {}'.format(t))
+            img = sess.run(img_var)
+            plt.imshow(deprocess_image(img[0], rescale=True))
+            plt.axis('off')
+            plt.show()
+    print('Iteration {}'.format(t))
+    img = sess.run(img_var)        
+    plt.imshow(deprocess_image(img[0], rescale=True))
+    plt.axis('off')
+    plt.show()
+
+# Composition VII + Tubingen
+params1 = {
+    'content_image' : 'styles/tubingen.jpg',
+    'style_image' : 'styles/composition_vii.jpg',
+    'image_size' : 192,
+    'style_size' : 512,
+    'content_layer' : 3,
+    'content_weight' : 5e-2, 
+    'style_layers' : (1, 4, 6, 7),
+    'style_weights' : (20000, 500, 12, 1),
+    'tv_weight' : 5e-2
+}
+
+style_transfer(**params1)
