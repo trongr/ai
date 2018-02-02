cdcedaff3ab61731083a2883fd81a633c3f3eb35
trong <trongr@users.noreply.github.com>
2018-02-01 22:12:52 -0500

    GAN: training

--- gan/gantest.py
+++ gan/gantest.py
@@ -129,21 +129,21 @@ def discriminator(x):
             activation_fn=leaky_relu,
             weights_initializer=tf.contrib.layers.xavier_initializer(),
             biases_initializer=tf.constant_initializer(0.1),
-            trainable=True) # poij Should this be True?
+            trainable=True) 
 
         fc2 = tf.contrib.layers.fully_connected(
             fc1, num_outputs=256, 
             activation_fn=leaky_relu,
             weights_initializer=tf.contrib.layers.xavier_initializer(),
             biases_initializer=tf.constant_initializer(0.1),
-            trainable=True) # poij Should this be True?
+            trainable=True) 
 
         logits = tf.contrib.layers.fully_connected(
             fc2, num_outputs=1, 
             activation_fn=None,
             weights_initializer=tf.contrib.layers.xavier_initializer(),
             biases_initializer=tf.constant_initializer(0.1),
-            trainable=True) # poij Should this be True?
+            trainable=True) 
 
         return logits
 
@@ -174,21 +174,21 @@ def generator(z):
             activation_fn=leaky_relu,
             weights_initializer=tf.contrib.layers.xavier_initializer(),
             biases_initializer=tf.constant_initializer(0.1),
-            trainable=True) # poij Should this be True?
+            trainable=True) 
 
         fc2 = tf.contrib.layers.fully_connected(
             fc1, num_outputs=1024, 
             activation_fn=leaky_relu,
             weights_initializer=tf.contrib.layers.xavier_initializer(),
             biases_initializer=tf.constant_initializer(0.1),
-            trainable=True) # poij Should this be True?
+            trainable=True) 
 
         img = tf.contrib.layers.fully_connected(
             fc2, num_outputs=784, 
             activation_fn=tf.tanh,
             weights_initializer=tf.contrib.layers.xavier_initializer(),
             biases_initializer=tf.constant_initializer(0.1),
-            trainable=True) # poij Should this be True?
+            trainable=True) 
 
         return img
 
@@ -217,22 +217,18 @@ def gan_loss(logits_real, logits_fake):
     - D_loss: discriminator loss scalar
     - G_loss: generator loss scalar
     """
-    labels_ones_g = tf.ones_like(logits_fake)
-    labels_ones_d = tf.ones_like(logits_real)
-    labels_zero_d = tf.zeros_like(logits_fake)
-
     G_loss = tf.reduce_mean(
                 tf.nn.sigmoid_cross_entropy_with_logits(
-                    labels=labels_ones_g, 
+                    labels=tf.ones_like(logits_fake), 
                     logits=logits_fake))
 
     D_loss = tf.reduce_mean(
                 tf.nn.sigmoid_cross_entropy_with_logits(
-                    labels=labels_ones_d, 
+                    labels=tf.ones_like(logits_real), 
                     logits=logits_real)) \
            + tf.reduce_mean(
                 tf.nn.sigmoid_cross_entropy_with_logits(
-                    labels=labels_zero_d, 
+                    labels=tf.zeros_like(logits_fake), 
                     logits=logits_fake))
 
     return D_loss, G_loss
@@ -262,3 +258,85 @@ def get_solvers(learning_rate=1e-3, beta1=0.5):
     G_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
     return D_solver, G_solver
 
+tf.reset_default_graph()
+
+# number of images for each batch
+batch_size = 128
+# our noise dimension
+noise_dim = 96
+
+# placeholder for images from the training dataset
+x = tf.placeholder(tf.float32, [None, 784])
+# random noise fed into our generator
+z = sample_noise(batch_size, noise_dim)
+# generated images
+G_sample = generator(z)
+
+with tf.variable_scope("") as scope:
+    #scale images to be -1 to 1
+    logits_real = discriminator(preprocess_img(x))
+    # Re-use discriminator weights on new inputs
+    scope.reuse_variables()
+    logits_fake = discriminator(G_sample)
+
+# Get the list of variables for the discriminator and generator
+D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator')
+G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'generator') 
+
+# get our solver
+D_solver, G_solver = get_solvers()
+
+# get our loss
+D_loss, G_loss = gan_loss(logits_real, logits_fake)
+
+# setup training steps
+D_train_step = D_solver.minimize(D_loss, var_list=D_vars)
+G_train_step = G_solver.minimize(G_loss, var_list=G_vars)
+D_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'discriminator')
+G_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'generator')
+
+# a giant helper function
+def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_extra_step,\
+              show_every=250, print_every=50, batch_size=128, num_epoch=10):
+    """Train a GAN for a certain number of epochs.
+    
+    Inputs:
+    - sess: A tf.Session that we want to use to run our data
+    - G_train_step: A training step for the Generator
+    - G_loss: Generator loss
+    - D_train_step: A training step for the Generator
+    - D_loss: Discriminator loss
+    - G_extra_step: A collection of tf.GraphKeys.UPDATE_OPS for generator
+    - D_extra_step: A collection of tf.GraphKeys.UPDATE_OPS for discriminator
+    Returns:
+        Nothing
+    """
+    # compute the number of iterations we need
+    max_iter = int(mnist.train.num_examples*num_epoch/batch_size)
+    for it in range(max_iter):
+        # every so often, show a sample result
+        if it % show_every == 0:
+            samples = sess.run(G_sample)
+            fig = show_images(samples[:16])
+            plt.show()
+            print()
+        # run a batch of data through the network
+        minibatch_x, minbatch_y = mnist.train.next_batch(batch_size)
+        _, D_loss_curr = sess.run([D_train_step, D_loss], 
+                            feed_dict={x: minibatch_x})
+        _, G_loss_curr = sess.run([G_train_step, G_loss])
+
+        # print loss every so often.
+        # We want to make sure D_loss doesn't go to 0
+        if it % print_every == 0:
+            print('Iter: {}, D: {:.4}, G:{:.4}'.format(it,D_loss_curr,G_loss_curr))
+    print('Final images')
+    samples = sess.run(G_sample)
+
+    fig = show_images(samples[:16])
+    plt.show()
+
+with get_session() as sess:
+    sess.run(tf.global_variables_initializer())
+    run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, 
+            G_extra_step, D_extra_step)
