69f1295bf8dc9a756a374f9ad285d3ddf8e23b67
trong <trongr@users.noreply.github.com>
2018-02-08 23:35:20 -0500

    GAN: deep convolutional GAN / DC-GAN: saving model

--- gan/dcgantest.py
+++ gan/dcgantest.py
@@ -9,11 +9,6 @@ plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-# number of images for each batch
-batch_size = 128
-# our noise dimension
-noise_dim = 96
-
 # A bunch of utility functions
 
 def show_images(images):
@@ -129,21 +124,30 @@ def discriminator(x):
     for an image being real for each input image.
     """
     with tf.variable_scope("discriminator"):
-        input_layer = tf.reshape(x, [-1, 28, 28, 1])
-        c1 = tf.layers.conv2d(inputs=input_layer, filters=32, 
-                kernel_size=5, strides=1, padding='same', 
-                activation=leaky_relu)
-        p1 = tf.layers.max_pooling2d(inputs=c1, pool_size=2, strides=2)
-        c2 = tf.layers.conv2d(inputs=p1, filters=64, kernel_size=5, strides=1, 
-                padding='same', activation=leaky_relu)
-        p2 = tf.layers.max_pooling2d(inputs=c2, pool_size=2, strides=2)
-        f1 = tf.reshape(p2, [-1, 7 * 7 * 64]) # For same padding
-        # f1 = tf.reshape(p2, [-1, 4 * 4 * 64]) # For valid padding
-        fc1 = tf.layers.dense(inputs=f1, units=1024, activation=leaky_relu)
-        logits = tf.layers.dense(inputs=fc1, units=1)
+        fc1 = tf.contrib.layers.fully_connected(
+            x, num_outputs=256, 
+            activation_fn=leaky_relu,
+            weights_initializer=tf.contrib.layers.xavier_initializer(),
+            biases_initializer=tf.constant_initializer(0.1),
+            trainable=True) 
+
+        fc2 = tf.contrib.layers.fully_connected(
+            fc1, num_outputs=256, 
+            activation_fn=leaky_relu,
+            weights_initializer=tf.contrib.layers.xavier_initializer(),
+            biases_initializer=tf.constant_initializer(0.1),
+            trainable=True) 
+
+        logits = tf.contrib.layers.fully_connected(
+            fc2, num_outputs=1, 
+            activation_fn=None,
+            weights_initializer=tf.contrib.layers.xavier_initializer(),
+            biases_initializer=tf.constant_initializer(0.1),
+            trainable=True) 
+
         return logits
 
-def test_discriminator(true_count):
+def test_discriminator(true_count=267009):
     tf.reset_default_graph()
     with get_session() as sess:
         y = discriminator(tf.ones((2, 784)))
@@ -153,8 +157,7 @@ def test_discriminator(true_count):
         else:
             print('Correct number of parameters in discriminator.')
         
-# test_discriminator(1102721) # valid padding in conv2d
-test_discriminator(3265409) # same padding in conv2d
+test_discriminator()
 
 def generator(z):
     """Generate images from a random noise vector.
@@ -166,76 +169,30 @@ def generator(z):
     TensorFlow Tensor of generated images, with shape [batch_size, 784].
     """
     with tf.variable_scope("generator"):
-        fc1 = tf.contrib.layers.fully_connected(z, num_outputs=1024, 
-                activation_fn=tf.nn.relu) 
-        bn1 = tf.layers.batch_normalization(
-                fc1,
-                axis=-1,
-                momentum=0.99, # use default
-                epsilon=0.001, # use default
-                center=True, # enable beta
-                scale=True, # enable gamma
-                training=True) 
-        # Adversarial is always training, unless you're using generator to
-        # generate images (which you can also do during training).
+        fc1 = tf.contrib.layers.fully_connected(
+            z, num_outputs=1024, 
+            activation_fn=leaky_relu,
+            weights_initializer=tf.contrib.layers.xavier_initializer(),
+            biases_initializer=tf.constant_initializer(0.1),
+            trainable=True) 
 
-        in_channels_1 = 128
-        fc2 = tf.contrib.layers.fully_connected(bn1, 
-                num_outputs=7 * 7 * in_channels_1, 
-                activation_fn=tf.nn.relu) 
-        bn2 = tf.layers.batch_normalization(
-                fc2,
-                axis=-1,
-                momentum=0.99, # use default
-                epsilon=0.001, # use default
-                center=True, # enable beta
-                scale=True, # enable gamma
-                training=True)
-        rs1 = tf.reshape(bn2, [-1, 7, 7, in_channels_1])
+        fc2 = tf.contrib.layers.fully_connected(
+            fc1, num_outputs=1024, 
+            activation_fn=leaky_relu,
+            weights_initializer=tf.contrib.layers.xavier_initializer(),
+            biases_initializer=tf.constant_initializer(0.1),
+            trainable=True) 
 
-        in_channels_2 = 64
-        ct1_filter = tf.get_variable(
-                        "ct1_filter",
-                        shape=(4, 4, in_channels_2, in_channels_1),
-                        dtype=tf.float32,
-                        initializer=tf.truncated_normal_initializer(),
-                        trainable=True)
-        ct1_filter = tf.ones((4, 4, in_channels_2, in_channels_1),
-                        dtype=tf.float32)
-        ct1 = tf.nn.relu(tf.nn.conv2d_transpose(
-                rs1,
-                filter=ct1_filter,
-                output_shape=(batch_size, 14, 14, in_channels_2),
-                strides=(1, 2, 2, 1),
-                padding='SAME',
-                data_format='NHWC'))
-
-        bn3 = tf.layers.batch_normalization(
-                ct1,
-                axis=-1, # batch normalize over channels (alt: axis=3)
-                momentum=0.99, # use default
-                epsilon=0.001, # use default
-                center=True, # enable beta
-                scale=True, # enable gamma
-                training=True)
-
-        ct2_filter = tf.get_variable(
-                        "ct2_filter",
-                        shape=(4, 4, 1, in_channels_2),
-                        dtype=tf.float32,
-                        initializer=tf.truncated_normal_initializer(),
-                        trainable=True)
-        img = tf.tanh(tf.nn.conv2d_transpose(
-                bn3,
-                filter=ct2_filter,
-                output_shape=(batch_size, 28, 28, 1),
-                strides=(1, 2, 2, 1),
-                padding='SAME',
-                data_format='NHWC'))
+        img = tf.contrib.layers.fully_connected(
+            fc2, num_outputs=784, 
+            activation_fn=tf.tanh,
+            weights_initializer=tf.contrib.layers.xavier_initializer(),
+            biases_initializer=tf.constant_initializer(0.1),
+            trainable=True) 
 
         return img
 
-def test_generator(true_count):
+def test_generator(true_count=1858320):
     tf.reset_default_graph()
     with get_session() as sess:
         y = generator(tf.ones((1, 4)))
@@ -245,7 +202,7 @@ def test_generator(true_count):
         else:
             print('Correct number of parameters in generator.')
         
-test_generator(6595521)
+test_generator()
 
 def gan_loss(logits_real, logits_fake):
     """Compute the GAN loss.
@@ -314,7 +271,7 @@ def test_lsgan_loss(score_real, score_fake, d_loss_true, g_loss_true):
 test_lsgan_loss(answers['logits_real'], answers['logits_fake'],
                 answers['d_loss_lsgan_true'], answers['g_loss_lsgan_true'])
 
-def get_solvers(dlr=1e-7, glr=1e-3, beta1=0.5):
+def get_solvers(learning_rate=1e-3, beta1=0.5):
     """Create solvers for GAN training.
     
     Inputs:
@@ -325,15 +282,20 @@ def get_solvers(dlr=1e-7, glr=1e-3, beta1=0.5):
     - D_solver: instance of tf.train.AdamOptimizer with correct learning_rate and beta1
     - G_solver: instance of tf.train.AdamOptimizer with correct learning_rate and beta1
     """
-    D_solver = tf.train.AdamOptimizer(learning_rate=dlr, beta1=beta1)
-    G_solver = tf.train.AdamOptimizer(learning_rate=glr, beta1=beta1)
+    D_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
+    G_solver = tf.train.AdamOptimizer(learning_rate=learning_rate, beta1=beta1)
     return D_solver, G_solver
 
 tf.reset_default_graph()
 
+# number of images for each batch
+batch_size = 128
+# our noise dimension
+noise_dim = 96
+
 # placeholder for images from the training dataset
 x = tf.placeholder(tf.float32, [None, 784])
-# random noise fed into our (generator)
+# random noise fed into our generator
 z = sample_noise(batch_size, noise_dim)
 # generated images
 G_sample = generator(z)
@@ -343,7 +305,7 @@ with tf.variable_scope("") as scope:
     logits_real = discriminator(preprocess_img(x))
     # Re-use discriminator weights on new inputs
     scope.reuse_variables()
-    logits_fake = discriminator(G_sample) 
+    logits_fake = discriminator(G_sample)
 
 # Get the list of variables for the discriminator and generator
 D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator')
@@ -354,17 +316,14 @@ D_solver, G_solver = get_solvers()
 
 # get our loss
 # TODO. Try different losses here
-# D_loss, G_loss = gan_loss(logits_real, logits_fake)
-D_loss, G_loss = lsgan_loss(logits_real, logits_fake)
+D_loss, G_loss = gan_loss(logits_real, logits_fake)
+# D_loss, G_loss = lsgan_loss(logits_real, logits_fake)
 
 # setup training steps
+D_train_step = D_solver.minimize(D_loss, var_list=D_vars)
+G_train_step = G_solver.minimize(G_loss, var_list=G_vars)
 D_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'discriminator')
-with tf.control_dependencies(D_extra_step):
-    D_train_step = D_solver.minimize(D_loss, var_list=D_vars)        
-
 G_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'generator')
-with tf.control_dependencies(G_extra_step):
-    G_train_step = G_solver.minimize(G_loss, var_list=G_vars)
 
 # a giant helper function
 def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_extra_step,\
@@ -382,6 +341,11 @@ def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_
     Returns:
         Nothing
     """
+    SAVE_DST = "dcgansave"
+    Saver = tf.train.Saver(max_to_keep=5, keep_checkpoint_every_n_hours=1) 
+        if glob.glob(SAVE_DST + "*"):
+            Saver.restore(sess, tf.train.latest_checkpoint(SAVE_DST))
+
     # compute the number of iterations we need
     max_iter = int(mnist.train.num_examples*num_epoch/batch_size)
     for it in range(max_iter):
@@ -401,6 +365,10 @@ def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_
         # We want to make sure D_loss doesn't go to 0
         if it % print_every == 0:
             print('Iter: {}, D: {:.4}, G:{:.4}'.format(it,D_loss_curr,G_loss_curr))
+
+        if it % 200 == 0:
+            Saver.save(sess, SAVE_DST, global_step=i)
+
     print('Final images')
     samples = sess.run(G_sample)
 
