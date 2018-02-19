567b8e6041ca993f6ee3854633fd2b8b90a6f2cf
trong <trongr@users.noreply.github.com>
2018-02-18 19:44:30 -0500

    GAN: clean up DC-GAN

--- gan/dcgantest.py
+++ gan/dcgantest.py
@@ -1,4 +1,5 @@
 from __future__ import print_function, division
+import os
 import tensorflow as tf
 import numpy as np
 import glob
@@ -10,12 +11,12 @@ plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-# number of images for each batch
 batch_size = 128
-# our noise dimension
 noise_dim = 96
 
-# A bunch of utility functions
+def mkdir_p(dir):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
 
 def save_images(dir, images, it):
     images = np.reshape(images, [images.shape[0], -1])  # images reshape to (batch_size, D)
@@ -37,30 +38,15 @@ def save_images(dir, images, it):
     print("Saving img " + imgpath)    
     fig.savefig(imgpath)
     plt.close(fig)
-    return
 
 def preprocess_img(x):
     return 2 * x - 1.0
-
-def deprocess_img(x):
-    return (x + 1.0) / 2.0
-
-def rel_error(x,y):
-    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
-
-def count_params():
-    """Count the number of parameters in the current TensorFlow graph """
-    param_count = np.sum([np.prod(x.get_shape().as_list()) for x in tf.global_variables()])
-    return param_count
-
 
 def get_session():
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     session = tf.Session(config=config)
     return session
-
-answers = np.load('gan-checks-tf.npz')
 
 from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('./cs231n/datasets/MNIST_data', one_hot=False)
@@ -77,15 +63,6 @@ def leaky_relu(x, alpha=0.01):
     """
     return tf.maximum(alpha * x, x)
 
-def test_leaky_relu(x, y_true):
-    tf.reset_default_graph()
-    with get_session() as sess:
-        y_tf = leaky_relu(tf.constant(x))
-        y = sess.run(y_tf)
-        print('Maximum error: %g'%rel_error(y_true, y))
-
-test_leaky_relu(answers['lrelu_x'], answers['lrelu_y'])
-
 def sample_noise(batch_size, dim):
     """Generate random uniform noise from -1 to 1.
     
@@ -98,26 +75,6 @@ def sample_noise(batch_size, dim):
     """
     return tf.random_uniform([batch_size, dim], minval=-1, maxval=1, 
                              dtype=tf.float32)
-
-def test_sample_noise():
-    batch_size = 3
-    dim = 4
-    tf.reset_default_graph()
-    with get_session() as sess:
-        z = sample_noise(batch_size, dim)
-        # Check z has the correct shape
-        assert z.get_shape().as_list() == [batch_size, dim]
-        # Make sure z is a Tensor and not a numpy array
-        assert isinstance(z, tf.Tensor)
-        # Check that we get different noise for different evaluations
-        z1 = sess.run(z)
-        z2 = sess.run(z)
-        assert not np.array_equal(z1, z2)
-        # Check that we get the correct range
-        assert np.all(z1 >= -1.0) and np.all(z1 <= 1.0)
-        print("All tests passed!")
-    
-test_sample_noise()
 
 def discriminator(x):
     """Compute discriminator score for a batch of input images.
@@ -143,19 +100,6 @@ def discriminator(x):
         fc1 = tf.layers.dense(inputs=f1, units=1024, activation=leaky_relu)
         logits = tf.layers.dense(inputs=fc1, units=1)
         return logits
-
-def test_discriminator(true_count=267009):
-    tf.reset_default_graph()
-    with get_session() as sess:
-        y = discriminator(tf.ones((2, 784)))
-        cur_count = count_params()
-        if cur_count != true_count:
-            print('Incorrect number of parameters in discriminator. {0} instead of {1}. Check your achitecture.'.format(cur_count,true_count))
-        else:
-            print('Correct number of parameters in discriminator.')
-        
-# test_discriminator(1102721) # valid padding in conv2d
-test_discriminator(3265409) # same padding in conv2d
 
 def generator(z):
     """Generate images from a random noise vector.
@@ -235,18 +179,6 @@ def generator(z):
                 data_format='NHWC'))
 
         return img
-        
-def test_generator(true_count=1858320):
-    tf.reset_default_graph()
-    with get_session() as sess:
-        y = generator(tf.ones((1, 4)))
-        cur_count = count_params()
-        if cur_count != true_count:
-            print('Incorrect number of parameters in generator. {0} instead of {1}. Check your achitecture.'.format(cur_count,true_count))
-        else:
-            print('Correct number of parameters in generator.')
-        
-test_generator(6595521)
 
 def gan_loss(logits_real, logits_fake):
     """Compute the GAN loss.
@@ -277,16 +209,6 @@ def gan_loss(logits_real, logits_fake):
 
     return D_loss, G_loss
 
-def test_gan_loss(logits_real, logits_fake, d_loss_true, g_loss_true):
-    tf.reset_default_graph()
-    with get_session() as sess:
-        d_loss, g_loss = sess.run(gan_loss(tf.constant(logits_real), tf.constant(logits_fake)))
-    print("Maximum error in d_loss: %g"%rel_error(d_loss_true, d_loss))
-    print("Maximum error in g_loss: %g"%rel_error(g_loss_true, g_loss))
-
-test_gan_loss(answers['logits_real'], answers['logits_fake'],
-              answers['d_loss_true'], answers['g_loss_true'])
-
 def lsgan_loss(score_real, score_fake):
     """Compute the Least Squares GAN loss.
     
@@ -305,60 +227,26 @@ def lsgan_loss(score_real, score_fake):
     G_loss = 0.5 * tf.reduce_mean((score_fake - 1) ** 2)
     return D_loss, G_loss
 
-def test_lsgan_loss(score_real, score_fake, d_loss_true, g_loss_true):
-    with get_session() as sess:
-        d_loss, g_loss = sess.run(
-            lsgan_loss(tf.constant(score_real), tf.constant(score_fake)))
-    print("lsgan Maximum error in d_loss: %g"%rel_error(d_loss_true, d_loss))
-    print("lsgan Maximum error in g_loss: %g"%rel_error(g_loss_true, g_loss))
-
-test_lsgan_loss(answers['logits_real'], answers['logits_fake'],
-                answers['d_loss_lsgan_true'], answers['g_loss_lsgan_true'])
-
-def get_solvers(dlr=5e-7, glr=1e-3, beta1=0.5):
-    """Create solvers for GAN training.
-    
-    Inputs:
-    - learning_rate: learning rate to use for both solvers
-    - beta1: beta1 parameter for both solvers (first moment decay)
-    
-    Returns:
-    - D_solver: instance of tf.train.AdamOptimizer with correct learning_rate and beta1
-    - G_solver: instance of tf.train.AdamOptimizer with correct learning_rate and beta1
-    """
-    D_solver = tf.train.AdamOptimizer(learning_rate=dlr, beta1=beta1)
-    G_solver = tf.train.AdamOptimizer(learning_rate=glr, beta1=beta1)
-    return D_solver, G_solver
-
 tf.reset_default_graph()
 
-# placeholder for images from the training dataset
 x = tf.placeholder(tf.float32, [None, 784])
-# random noise fed into our generator
 z = sample_noise(batch_size, noise_dim)
-# generated images
 G_sample = generator(z)
 
 with tf.variable_scope("") as scope:
-    #scale images to be -1 to 1
     logits_real = discriminator(preprocess_img(x))
-    # Re-use discriminator weights on new inputs
-    scope.reuse_variables()
+    scope.reuse_variables() # Re-use discriminator weights on new inputs
     logits_fake = discriminator(G_sample)
 
-# Get the list of variables for the discriminator and generator
 D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator')
 G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'generator') 
 
-# get our solver
-D_solver, G_solver = get_solvers()
-
-# get our loss
-# TODO. Try different losses here
+beta1 = 0.5
+D_solver = tf.train.AdamOptimizer(learning_rate=5e-7, beta1=beta1)
+G_solver = tf.train.AdamOptimizer(learning_rate=1e-3, beta1=beta1)
 # D_loss, G_loss = gan_loss(logits_real, logits_fake)
 D_loss, G_loss = lsgan_loss(logits_real, logits_fake)
 
-# setup training steps
 D_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'discriminator')
 with tf.control_dependencies(D_extra_step):
     D_train_step = D_solver.minimize(D_loss, var_list=D_vars)
@@ -367,9 +255,9 @@ G_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'generator')
 with tf.control_dependencies(G_extra_step):
     G_train_step = G_solver.minimize(G_loss, var_list=G_vars)
 
-# a giant helper function
-def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_extra_step,\
-              show_every=250, print_every=50, batch_size=128, num_epoch=10):
+def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, 
+        D_extra_step, save_img_every=250, print_every=50, batch_size=128, 
+        num_epoch=10):
     """Train a GAN for a certain number of epochs.
     
     Inputs:
@@ -383,35 +271,32 @@ def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_
     Returns:
         Nothing
     """
-    SAVE_DST = "dcgansave"
+    out_dir = "out"
+    save_dir = "save"
+    mkdir_p(out_dir)
+    mkdir_p(save_dir)
+
     Saver = tf.train.Saver(max_to_keep=5, keep_checkpoint_every_n_hours=1) 
-    if glob.glob(SAVE_DST + "/*"):
-        Saver.restore(sess, tf.train.latest_checkpoint(SAVE_DST))
+    if glob.glob(save_dir + "/*"):
+        Saver.restore(sess, tf.train.latest_checkpoint(save_dir))
 
-    # compute the number of iterations we need
-    max_iter = int(mnist.train.num_examples*num_epoch/batch_size)
+    max_iter = int(mnist.train.num_examples * num_epoch / batch_size)
     for it in range(max_iter):
-        # every so often, save a sample to disk
-        if it % show_every == 0:
-            samples = sess.run(G_sample)
-            save_images("dcganimgs01", samples[:49], it)
-
-        # run a batch of data through the network
-        minibatch_x, minbatch_y = mnist.train.next_batch(batch_size)
-        _, D_loss_curr = sess.run([D_train_step, D_loss], 
-                            feed_dict={x: minibatch_x})
+        xmb, _ = mnist.train.next_batch(batch_size)
+        _, D_loss_curr = sess.run([D_train_step, D_loss], feed_dict={x: xmb})
         _, G_loss_curr = sess.run([G_train_step, G_loss])
 
-        # print loss every so often.
-        # We want to make sure D_loss doesn't go to 0
-        if it % print_every == 0:
-            print('Iter: {}, D: {:.4}, G: {:.4}'.format(it, D_loss_curr, 
-                G_loss_curr))
+        if it % save_img_every == 0:
+            samples = sess.run(G_sample)
+            save_images(out_dir, samples[:49], it)
+
+        if it % print_every == 0: # We want to make sure D_loss doesn't go to 0
+            print('Iter: {}, D: {:.4}, G: {:.4}'.format(it, D_loss_curr, G_loss_curr))
 
         if it % 10 == 0:
-            Saver.save(sess, SAVE_DST + "/dcgan", global_step=it)
+            Saver.save(sess, save_dir + "/gan", global_step=it)
 
 with get_session() as sess:
     sess.run(tf.global_variables_initializer())
     run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, 
-            G_extra_step, D_extra_step, show_every=200, num_epoch=1000)
+        G_extra_step, D_extra_step, save_img_every=200, num_epoch=1000)
