507d465cedf77a4c4d89f32988f2273637276a59
trong <trongr@users.noreply.github.com>
2018-03-03 22:14:30 -0500

    GAN: try conv stride 2

--- gan/conv-dropout-g-conv-d-softmax.py
+++ gan/conv-dropout-g-conv-d-softmax.py
@@ -139,18 +139,19 @@ def generator(z, keep_prob):
 
         dr2_1 = tf.nn.dropout(rs1, keep_prob)   
         c1 = tf.layers.conv2d(inputs=dr2_1, filters=16, 
-                kernel_size=5, strides=1, padding='same', 
+                kernel_size=5, strides=2, padding='same', 
                 activation=leaky_relu)
         p1 = tf.layers.max_pooling2d(inputs=c1, pool_size=2, strides=2) 
 
         dr2_2 = tf.nn.dropout(p1, keep_prob)   
         c2 = tf.layers.conv2d(inputs=dr2_2, filters=32, 
-                kernel_size=5, strides=1, padding='same', 
+                kernel_size=5, strides=2, padding='same', 
                 activation=leaky_relu)
-        p2 = tf.layers.max_pooling2d(inputs=c2, pool_size=2, strides=2)         
+        p2 = tf.layers.max_pooling2d(inputs=c2, pool_size=2, strides=2)     
 
-        c3 = tf.layers.conv2d(inputs=p2, filters=64, 
-                kernel_size=5, strides=1, padding='same', 
+        dr2_3 = tf.nn.dropout(p2, keep_prob)               
+        c3 = tf.layers.conv2d(inputs=dr2_3, filters=64, 
+                kernel_size=5, strides=2, padding='same', 
                 activation=leaky_relu)
 
         avg = tf.reduce_mean(c3, axis=3, keep_dims=True)
@@ -158,7 +159,7 @@ def generator(z, keep_prob):
         rs2 = tf.reshape(x, [-1, x_dim])
         # Need this last FC layer cause for some reason you can't have reshape
         # as the last layer cause it has no gradient.
-        dr3 = tf.nn.dropout(rs2, keep_prob) # poij try taking this out           
+        dr3 = tf.nn.dropout(rs2, keep_prob)       
         img = tf.contrib.layers.fully_connected(dr3, num_outputs=x_dim, 
                 activation_fn=tf.tanh) 
 
@@ -173,11 +174,11 @@ with tf.name_scope('input'):
     x = tf.placeholder(tf.float32, shape=[None, x_dim], name="x-input")
     z = tf.placeholder(tf.float32, shape=[None, noise_dim], name="z-input")
     keep_prob = tf.placeholder(tf.float32, name='keep_prob')
-G_sample = generator(z, keep_prob)
 
 with tf.variable_scope("") as scope:
+    G_sample = generator(z, keep_prob)    
     D_real = discriminator(preprocess_img(x)) # scale images to be -1 to 1
-    scope.reuse_variables() # Re-use discriminator weights on new inputs
+    scope.reuse_variables() # Re-use discriminator weights on new inputs        
     D_fake = discriminator(G_sample)
 
 D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'discriminator')
@@ -193,16 +194,11 @@ tf.summary.scalar("D_loss", D_loss)
 tf.summary.scalar("G_loss", G_loss)
 summary_op = tf.summary.merge_all()
 
-dlr, glr = 1e-3, 1e-3
-beta1 = 0.5
-D_solver = tf.train.AdamOptimizer(learning_rate=dlr, beta1=beta1)
-G_solver = tf.train.AdamOptimizer(learning_rate=glr, beta1=beta1)
-
-D_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'discriminator')
-G_extra_step = tf.get_collection(tf.GraphKeys.UPDATE_OPS, 'generator')
-with tf.control_dependencies(D_extra_step):
+dlr, glr, beta1 = 1e-3, 1e-3, 0.5
+with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
+    D_solver = tf.train.AdamOptimizer(learning_rate=dlr, beta1=beta1)
     D_train_step = D_solver.minimize(D_loss, var_list=D_vars)
-with tf.control_dependencies(G_extra_step):
+    G_solver = tf.train.AdamOptimizer(learning_rate=glr, beta1=beta1)
     G_train_step = G_solver.minimize(G_loss, var_list=G_vars)
 
 out_dir = "out"
@@ -215,7 +211,7 @@ mkdir_p(save_dir)
 
 writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
 
-def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_extra_step,\
+def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss,
               show_every=250, print_every=50, batch_size=128, num_epoch=10):
     Saver = tf.train.Saver(max_to_keep=5, keep_checkpoint_every_n_hours=1) 
     if glob.glob(save_dir + "/*"):
@@ -253,4 +249,4 @@ def run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, G_extra_step, D_
 with get_session() as sess:
     sess.run(tf.global_variables_initializer())
     run_a_gan(sess, G_train_step, G_loss, D_train_step, D_loss, 
-            G_extra_step, D_extra_step, show_every=50, num_epoch=1000)
+        show_every=50, num_epoch=1000)
